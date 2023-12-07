b90179a56f3ceb1fe2c3e005134f9f7a77500988
Matheus Leal Gomes da Silva <matheusleal@tre-pa.jus.br>
2023-12-07 14:54:21 -0300

    feat: contador com led

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,10 @@ const byte two[] = {B111111, B111111, B111111, B000000, B111111, B111111, B00000
 const byte three[] = {B111111, B111111, B111111, B000000, B000000, B111111, B111111}; 
 const byte four[] = {B111111, B111111, B000000, B111111, B000000, B000000, B111111};
 const byte five[] = {B111111, B000000, B111111, B111111, B000000, B111111, B111111};
+const byte six[] = {B111111, B000000, B111111, B111111, B1111111, B111111, B111111};
+const byte seven[] = {B000000, B111111, B111111, B000000, B000000, B000000, B111111}; 
+const byte eight[] = {B111111, B111111, B111111, B111111, B111111, B111111, B111111}; 
+const byte nine[] = {B111111, B111111, B111111, B111111, B000000, B111111, B111111}; 
 
 void setup() {
   #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
@@ -22,12 +26,16 @@ void setup() {
 }
 
 void loop() {
-  //printNumber(zero, sizeof(zero) / sizeof(zero[0]), pixels.Color(0, 150, 0));
-  //printNumber(one, sizeof(one) / sizeof(one[0]), pixels.Color(0, 0, 150));
-  //printNumber(two, sizeof(two) / sizeof(two[0]), pixels.Color(0, 100, 150));
-  //printNumber(three, sizeof(three) / sizeof(three[0]), pixels.Color(150, 0, 150));
-  //printNumber(four, sizeof(four) / sizeof(four[0]), pixels.Color(150, 0, 150));
-  printNumber(five, sizeof(five) / sizeof(five[0]), pixels.Color(150, 0, 150));
+  printNumber(zero, sizeof(zero) / sizeof(zero[0]), pixels.Color(255, 0, 0));
+  printNumber(one, sizeof(one) / sizeof(one[0]), pixels.Color(0, 255, 0));
+  printNumber(two, sizeof(two) / sizeof(two[0]), pixels.Color(0, 0, 255));
+  printNumber(three, sizeof(three) / sizeof(three[0]), pixels.Color(255, 255, 0));
+  printNumber(four, sizeof(four) / sizeof(four[0]), pixels.Color(0, 255, 255));
+  printNumber(five, sizeof(five) / sizeof(five[0]), pixels.Color(255, 0, 255));
+  printNumber(six, sizeof(six) / sizeof(six[0]), pixels.Color(255, 165, 0));
+  printNumber(seven, sizeof(seven) / sizeof(seven[0]), pixels.Color(0, 128, 0));
+  printNumber(eight, sizeof(eight) / sizeof(eight[0]), pixels.Color(128, 0, 128));
+  printNumber(nine, sizeof(nine) / sizeof(nine[0]), pixels.Color(150, 0, 150));
 }
 
 void printNumber(const byte* number, int length, uint32_t color) {
